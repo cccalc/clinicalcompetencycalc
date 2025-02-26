3df88f62468a0b381ea3fe5970432ddd1bd342a8
Tythran <117491821+Tythran@users.noreply.github.com>
2025-02-26 14:20:31 -0500

    Trying a different type of routing

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -20,6 +20,7 @@ const supabase = createClient();
  */
 const Header = () => {
   const pathname = usePathname();
+  const router = useRouter();
   const [showProfileMenu, setShowProfileMenu] = useState(false);
   const profileMenuRef = useRef<HTMLDivElement>(null);
 
