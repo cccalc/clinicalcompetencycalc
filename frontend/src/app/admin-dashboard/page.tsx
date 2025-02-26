2c7831dee2d7cb5b4c8c37dbcaef905d4e5e77eb
Tythran <117491821+Tythran@users.noreply.github.com>
2025-02-26 13:57:04 -0500

    Added Header to All Users Page

--- frontend/src/app/admin-dashboard/page.tsx
+++ frontend/src/app/admin-dashboard/page.tsx
@@ -1,10 +1,12 @@
 'use client';
 
 import AdminDashboard from '@/app/admin-dashboard/adminDashboard';
+import Header from '@/components/header';
 
 const AdminDashboardPage = () => {
   return (
     <>
+      <Header />
       <AdminDashboard />
     </>
   );
