b8286f8e9a30ac4eb1cc4d6920eb2e3eddc1baab
atlantisel <80100913+atlantisel@users.noreply.github.com>
2025-03-05 03:16:59 -0500

    feat: loaded bert model training layers PYTHON NEEDS TO DIE

--- python/bert/init.sh
+++ python/bert/init.sh
@@ -2,4 +2,4 @@ rm -rf .venv
 python3.11 -m venv .venv
 source .venv/bin/activate
 pip install --upgrade pip
-pip install -r requirements.txt
+# pip install -r requirements.txt
--- python/bert/train.py
+++ python/bert/train.py
@@ -6,10 +6,13 @@ import tensorflow_hub as hub
 import tensorflow_text as text
 from official.nlp import optimization  # to create AdamW optimizer
 
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
+
+os.environ['TF_USE_LEGACY_KERAS'] = '1'
 
 
 def main() -> None:
+  """Main function."""
   tf.get_logger().setLevel('ERROR')
 
   AUTOTUNE = tf.data.AUTOTUNE
@@ -45,16 +48,38 @@ def main() -> None:
   tfhub_handle_encoder = 'https://tfhub.dev/google/experts/bert/pubmed/2'
   tfhub_handle_preprocess = 'https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/3'
 
-  bert_preprocess_model = hub.KerasLayer(tfhub_handle_preprocess)
+  # bert_preprocess_model = hub.KerasLayer(tfhub_handle_preprocess)
+  # bert_model = hub.KerasLayer(tfhub_handle_encoder)
 
   text_test = ['Demonstrates situational awareness when discussing patients']
-  text_preprocessed = bert_preprocess_model(text_test)
+  # text_preprocessed = bert_preprocess_model(text_test)
 
-  print(f'Keys       : {list(text_preprocessed.keys())}')
-  print(f'Shape      : {text_preprocessed["input_word_ids"].shape}')
-  print(f'Word Ids   : {text_preprocessed["input_word_ids"][0, :12]}')
-  print(f'Input Mask : {text_preprocessed["input_mask"][0, :12]}')
-  print(f'Type Ids   : {text_preprocessed["input_type_ids"][0, :12]}')
+  classifier_model = buildClassifierModel(tfhub_handle_encoder, tfhub_handle_preprocess)
+  bert_raw_result = classifier_model(tf.constant(text_test))
+  print(tf.sigmoid(bert_raw_result))
+
+
+def buildClassifierModel(tfhub_handle_encoder: str, tfhub_handle_preprocess: str) -> tf.keras.Model:
+  """
+  Builds a classifier model using the BERT encoder and preprocess layers.
+
+  :param tfhub_handle_encoder:
+  :type tfhub_handle_encoder: str
+  :param tfhub_handle_preprocess:
+  :type tfhub_handle_preprocess: str
+  :return:
+  :rtype: tf.keras.Model
+  """
+
+  text_input = tf.keras.layers.Input(shape=(), dtype=tf.string, name='text')
+  preprocessing_layer = hub.KerasLayer(tfhub_handle_preprocess, name='preprocessing')
+  encoder_inputs = preprocessing_layer(text_input)
+  encoder = hub.KerasLayer(tfhub_handle_encoder, trainable=True, name='BERT_encoder')
+  outputs = encoder(encoder_inputs)
+  net = outputs['pooled_output']
+  net = tf.keras.layers.Dropout(0.1)(net)
+  net = tf.keras.layers.Dense(1, activation=None, name='classifier')(net)
+  return tf.keras.Model(text_input, net)
 
 
 if __name__ == '__main__':
