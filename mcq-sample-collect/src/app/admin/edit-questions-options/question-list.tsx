c3d8d204e52322761bfc63b401d089478ec9d4d0
atlantisel <80100913+atlantisel@users.noreply.github.com>
2025-03-12 19:48:57 -0400

    feat: implement new edit question modal components; added comments, slight refactoring

--- mcq-sample-collect/src/app/admin/edit-questions-options/question-list.tsx
+++ mcq-sample-collect/src/app/admin/edit-questions-options/question-list.tsx
@@ -7,20 +7,23 @@ import { getHistoricalMCQs } from '@/utils/get-epa-data';
 import type { Tables } from '@/utils/supabase/database.types';
 import type { MCQ } from '@/utils/types';
 
-import EditOptionModal from './edit-option-modal';
-import EditQuestionModal from './edit-question-modal';
+import EditOptionModal from './edit-modal-option';
+import EditQuestionModal from './edit-modal-question';
 import QuestionItem from './question-list-item';
 
 export default function QuestionList() {
-  const [mcqInformation, setMCQInformation] = useState<Tables<'mcqs_options'>[] | null>(null);
+  const [mcqsInformation, setMCQsInformation] = useState<Tables<'mcqs_options'>[] | null>(null);
 
   const [optionMCQ, setOptionMCQ] = useState<MCQ | null>(null);
   const [optionKey, setOptionKey] = useState<string | null>(null);
   const [optionText, setOptionText] = useState<string | null>(null);
   const [newOptionText, setNewOptionText] = useState<string | null>(null);
 
+  const [questionMCQ, setQuestionMCQ] = useState<MCQ | null>(null);
+  const [newQuestionText, setNewQuestionText] = useState<string | null>(null);
+
   useEffect(() => {
-    const fetchMCQs = async () => setMCQInformation((await getHistoricalMCQs()) ?? null);
+    const fetchMCQs = async () => setMCQsInformation((await getHistoricalMCQs()) ?? null);
     fetchMCQs();
   }, []);
 
@@ -34,32 +37,44 @@ export default function QuestionList() {
     });
   };
 
+  const handleQuestionClick = (mcq: MCQ) => {
+    setQuestionMCQ(mcq);
+    setNewQuestionText(mcq.question);
+    document.querySelectorAll('input[id="new-question"]').forEach((el) => {
+      (el as HTMLInputElement).value = mcq.question;
+    });
+  };
+
   return (
     <>
       <h3 className='mb-3'>Edit form questions and options</h3>
-      {!mcqInformation || !mcqInformation.length || !mcqInformation![0].data ? (
+      {!mcqsInformation || !mcqsInformation.length || !mcqsInformation![0].data ? (
         <Loading />
       ) : (
         <div className='accordion' id='question-list'>
-          {(mcqInformation[0].data as MCQ[]).map((mcq, i) => (
-            <QuestionItem key={i} i={i} mcq={mcq} handleOptionClick={handleOptionClick} />
+          {(mcqsInformation[0].data as MCQ[]).map((mcq, i) => (
+            <QuestionItem
+              key={i}
+              i={i}
+              mcq={mcq}
+              handleOptionClick={handleOptionClick}
+              handleQuestionClick={handleQuestionClick}
+            />
           ))}
         </div>
       )}
 
       <EditOptionModal
-        mcqInformation={{ get: mcqInformation, set: setMCQInformation }}
+        mcqsInformation={{ get: mcqsInformation, set: setMCQsInformation }}
         optionMCQ={{ get: optionMCQ, set: setOptionMCQ }}
         optionKey={{ get: optionKey, set: setOptionKey }}
         optionText={{ get: optionText, set: setOptionText }}
         newOptionText={{ get: newOptionText, set: setNewOptionText }}
       />
       <EditQuestionModal
-        mcqInformation={{ get: mcqInformation, set: setMCQInformation }}
-        optionMCQ={{ get: optionMCQ, set: setOptionMCQ }}
-        optionKey={{ get: optionKey, set: setOptionKey }}
-        optionText={{ get: optionText, set: setOptionText }}
-        newOptionText={{ get: newOptionText, set: setNewOptionText }}
+        mcqsInformation={{ get: mcqsInformation, set: setMCQsInformation }}
+        questionMCQ={{ get: questionMCQ, set: setQuestionMCQ }}
+        newQuestionText={{ get: newQuestionText, set: setNewQuestionText }}
       />
     </>
   );
--- mcq-sample-collect/src/utils/types.ts
+++ mcq-sample-collect/src/utils/types.ts
@@ -13,10 +13,10 @@ export type MCQ = {
 
 export type DevLevel = 'none' | 'remedial' | 'early-developing' | 'developing' | 'entrustable';
 
-export type OptHistoryInstance = {
+export type changeHistoryInstance = {
   updated_at: Date;
   updated_by: string;
   updater_display_name?: string | null;
   updater_email?: string | null;
-  option: string;
+  text: string;
 };
