7a341e206c12c0197efc7bfc1b32ea738c7eb4cc
Dylan Colburn <djc6533@psu.edu>
2025-03-12 22:18:55 -0400

    Update python_server.py

--- server/socketio/python_server.py
+++ server/socketio/python_server.py
@@ -1,6 +1,7 @@
+import threading
 import socketio
 import eventlet
-import threading
+
 
 sio = socketio.Client()
 server_sio = socketio.Server()
@@ -10,40 +11,40 @@ connected = threading.Event()
 
 @sio.event
 def connect():
-    print('Connection established with Node.js server')
-    connected.set() 
+  print('Connection established with Node.js server')
+  connected.set() 
 
 @sio.event
 def message(data):
-    print('\nMessage from Node.js server:', data)
-    print('Enter a message: ', end='', flush=True)
+  print('\nMessage from Node.js server:', data)
+  print('Enter a message: ', end='', flush=True)
 
 @sio.event
 def disconnect():
-    print('Disconnected from Node.js server')
-    connected.clear()
+  print('Disconnected from Node.js server')
+  connected.clear()
 
 @server_sio.event
 def connect(sid, environ):
-    print(f'Python server connected to a client: {sid}')
+  print(f'Python server connected to a client: {sid}')
 
 @server_sio.event
 def message(sid, data):
-    print('\nMessage from Node.js:', data)
+  print('\nMessage from Node.js:', data)
 
     # Avoid infinite loop by not re-emitting the message received from Node.js
-    if data != "forwarded":
-        sio.emit('message', "forwarded")
+  if data != "forwarded":
+    sio.emit('message', "forwarded")
 
 def send_message():
-    while True:
-        message = input('Enter a message: ')
-        connected.wait()
-        sio.emit('message', message)
+  while True:
+    message = input('Enter a message: ')
+    connected.wait()
+    sio.emit('message', message)
 
-if __name__ == '__main__':
-    threading.Thread(target=send_message, daemon=True).start()
+if __name__=='__main__':
+  threading.Thread(target=send_message, daemon=True).start()
 
-    sio.connect('http://localhost:3000')
+  sio.connect('http://localhost:3000')
 
-    eventlet.wsgi.server(eventlet.listen(('', 5000)), app)
+  eventlet.wsgi.server(eventlet.listen(('', 5000)), app)
