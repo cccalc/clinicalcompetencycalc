d5ef652d615b7b44fd62ae327a2f6caa94159bdd
Tythran <117491821+Tythran@users.noreply.github.com>
2025-03-13 19:55:42 -0400

    feature: Added an admin dashboard component to show notifications, complete with tags and filtering, it uses dummy data currently.

--- frontend/src/components/(AdminComponents)/adminDashboard.tsx
+++ frontend/src/components/(AdminComponents)/adminDashboard.tsx
@@ -1,10 +1,13 @@
 'use client';
 
+import Notifications from '@/components/(AdminComponents)/notifications';
+
 const AdminDashboardPage = () => {
   return (
     <>
       <h1>Hello, you are an admin.</h1>
       <p>Here you can manage users, view reports, and perform admin tasks.</p>
+      <Notifications />
     </>
   );
 };
