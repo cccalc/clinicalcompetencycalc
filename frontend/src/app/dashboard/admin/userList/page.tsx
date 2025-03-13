c898747012d93c5423caf4202dee60780485b180
Tythran <117491821+Tythran@users.noreply.github.com>
2025-03-13 19:14:32 -0400

    feature: updated user list on the admin page

--- frontend/src/app/dashboard/admin/userList/page.tsx
+++ frontend/src/app/dashboard/admin/userList/page.tsx
@@ -307,6 +307,31 @@ const AdminDashboard = () => {
           </div>
         </div>
       )}
+      {showDeactivateModal && selectedUser && (
+        <div className='modal show' tabIndex={-1} style={{ display: 'block' }}>
+          <div className='modal-dialog'>
+            <div className='modal-content rounded shadow-lg'>
+              <div className='modal-header bg-danger text-white'>
+                <h5 className='modal-title'>Confirm Delete</h5>
+                <button type='button' className='btn-close' onClick={() => setShowDeactivateModal(false)}></button>
+              </div>
+              <div className='modal-body text-center'>
+                <p>
+                  Are you sure you want to delete <strong>{selectedUser.display_name}</strong>?
+                </p>
+              </div>
+              <div className='modal-footer'>
+                <button type='button' className='btn btn-secondary' onClick={() => setShowDeactivateModal(false)}>
+                  Cancel
+                </button>
+                <button type='button' className='btn btn-danger'>
+                  Delete
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
