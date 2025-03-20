6f26b0aba4bdf35c7700cab4f017659de3c57c27
Dylan Colburn <djc6533@psu.edu>
2025-03-20 13:06:54 -0400

    Date fix
    fixes the date

---
+++ frontend/src/components/(AdminComponents)/adminDashboard 2.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import Notifications from '@/components/(AdminComponents)/notifications';
+
+const AdminDashboardPage = () => {
+  return (
+    <>
+      <Notifications />
+    </>
+  );
+};
+
+export default AdminDashboardPage;
