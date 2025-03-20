862f214a19325ad125eebe95241463a71a8c9cfa
atlantisel <80100913+atlantisel@users.noreply.github.com>
2025-03-20 15:11:02 -0400

    feat: implement direct conversion from database fetch to Keras format

---
+++ python/bert/supabase_to_keras.py
@@ -0,0 +1,48 @@
+import os
+import sys
+
+import pandas as pd
+
+from dotenv import load_dotenv
+from supabase import Client, create_client
+from csv_to_keras import exportKerasFolder
+
+def main(argv: list[str]) -> None:
+
+  # Check if the correct number of arguments is provided
+  if len(argv) != 3:
+    print("Usage: python supabase_to_keras.py <dataset_name> <training_split>")
+    sys.exit(1)
+  dataset_name = argv[1]
+
+  if not dataset_name:
+    raise ValueError("Dataset name cannot be empty.")
+
+  training_split = float(argv[2])
+
+  if not 0 <= training_split <= 1:
+    raise ValueError("Training split must be between 0 and 1.")
+
+  # Load environment variables from .env file
+  load_dotenv()
+
+  url: str = os.environ.get("SUPABASE_URL", "")
+  key: str = os.environ.get("SUPABASE_SERVICE_ROLE_KEY", "")
+
+  if url == "" or key == "":
+    raise ValueError("Supabase URL or key not found in environment variables.")
+
+  supabase: Client = create_client(url, key)
+
+  response = supabase.schema("trainingdata")   \
+                     .table("text_responses")  \
+                     .select("*")              \
+                     .execute()
+
+  df = pd.DataFrame(response.data)
+
+  keras_directory = os.path.join(os.getcwd(), 'data', 'keras', f'{dataset_name}-{training_split}')
+  exportKerasFolder(df, keras_directory, training_split=training_split)
+
+if __name__ == "__main__":
+  main(sys.argv)
