9f88acf3f89317cf71bf94a2bbd578d15d07481d
Tythran <117491821+Tythran@users.noreply.github.com>
2025-03-29 19:52:49 -0400

    feature: Announcements have been completed

--- frontend/src/components/(AdminComponents)/adminDashboard.tsx
+++ frontend/src/components/(AdminComponents)/adminDashboard.tsx
@@ -1,12 +1,14 @@
 'use client';
 
 import Notifications from '@/components/(AdminComponents)/notifications';
+import AdminAnnouncements from './AdminAnnouncements';
 
 const AdminDashboardPage = () => {
   return (
-    <>
+    <div className='d-flex gap-3 flex-wrap'>
       <Notifications />
-    </>
+      <AdminAnnouncements />
+    </div>
   );
 };
 
