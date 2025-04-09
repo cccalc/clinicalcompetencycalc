8d52d19768ab37dacac44591a994fac5ce460cb0
Dylan Colburn <djc6533@psu.edu>
2025-04-09 19:56:33 -0400

    feature/addedDocker

--- python/infer/init.sh
+++ python/infer/init.sh
@@ -1,6 +1,6 @@
-rm -rf .venv
+
 python3.11 -m venv .venv
-source .venv/bin/activate
+.  .venv/bin/activate
 pip install --upgrade pip
 pip install "tensorflow-text==2.15.*"
 pip install "tensorflow=2.15.*"
