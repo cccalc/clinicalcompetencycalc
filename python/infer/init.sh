23f77a75baaf4bf6039b78310d5938a8873131d6
atlantisel <80100913+atlantisel@users.noreply.github.com>
2025-04-08 22:24:18 -0400

    feat: update init.sh to install specific TensorFlow packages and additional dependencies

--- python/infer/init.sh
+++ python/infer/init.sh
@@ -2,5 +2,15 @@ rm -rf .venv
 python3.11 -m venv .venv
 source .venv/bin/activate
 pip install --upgrade pip
-pip install -r requirements.txt
+pip install "tensorflow-text==2.15.*"
+pip install "tensorflow=2.15.*"
+pip install "tf-keras==2.15.*"
+pip install "tf-models-official==2.15.*"
+pip install dotenv
+pip install kaggle
+pip install keras-tuner
+pip install matplotlib
+pip install pandas
+pip install scikit-learn
+pip install supabase
 kaggle kernels output cccalc/bert-training -p bert-model
